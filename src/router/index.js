cb77851536713da76e4bd22e9b1452edc5288234
Arraybuffer <arraybuffer@foxmail.com>
2018-11-08 15:11:47 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,9 @@ Vue.use(Router)
 
 // route-level code splitting
 const createListView = id => () => import('../views/CreateListView').then(m => m.default(id))
-const ItemView = () => import('../views/ItemView.vue')
-const UserView = () => import('../views/UserView.vue')
+// const ItemView = () => import('../views/ItemView.vue')
+// const UserView = () => import('../views/UserView.vue')
+import HelloWorld from '../components/HelloWorld.vue'
 
 export function createRouter () {
   return new Router({
@@ -14,14 +15,14 @@ export function createRouter () {
     fallback: false,
     scrollBehavior: () => ({ y: 0 }),
     routes: [
-      { path: '/top/:page(\\d+)?', component: createListView('top') },
-      { path: '/new/:page(\\d+)?', component: createListView('new') },
-      { path: '/show/:page(\\d+)?', component: createListView('show') },
-      { path: '/ask/:page(\\d+)?', component: createListView('ask') },
-      { path: '/job/:page(\\d+)?', component: createListView('job') },
-      { path: '/item/:id(\\d+)', component: ItemView },
-      { path: '/user/:id', component: UserView },
-      { path: '/', redirect: '/top' }
+      { path: '/', component: HelloWorld },
+      // { path: '/top/:page(\\d+)?', component: createListView('top') },
+      // { path: '/new/:page(\\d+)?', component: createListView('new') },
+      // { path: '/show/:page(\\d+)?', component: createListView('show') },
+      { path: '/ask/:page(\\d+)?', component: createListView('ask') }
+      // { path: '/job/:page(\\d+)?', component: createListView('job') },
+      // { path: '/item/:id(\\d+)', component: ItemView },
+      // { path: '/user/:id', component: UserView }
     ]
   })
 }
